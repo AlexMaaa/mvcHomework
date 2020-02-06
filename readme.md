0d5107e9f4a8850d739d64d813edb3c136174718
麻津铭 <ma.jinming@ielpm.com>
2020-02-06 16:57:03 +0800

    mvc作业

---
+++ readme.md
@@ -0,0 +1,2 @@
+zhangsan: /demo/zhangsanQuery
+lisi: /demo/lisiQuery
---
+++ src/main/java/com/lagou/demo/controller/DemoController.java
@@ -0,0 +1,46 @@
+package com.lagou.demo.controller;
+
+import com.lagou.demo.service.IDemoService;
+import com.lagou.edu.mvcframework.annotations.LagouAutowired;
+import com.lagou.edu.mvcframework.annotations.LagouController;
+import com.lagou.edu.mvcframework.annotations.LagouRequestMapping;
+import com.lagou.edu.mvcframework.annotations.LagouSecurity;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@LagouController
+@LagouRequestMapping("/demo")
+public class DemoController {
+
+
+    @LagouAutowired
+    private IDemoService demoService;
+
+
+    /**
+     * URL: /demo/query?name=lisi
+     *
+     * @param request
+     * @param response
+     * @param name
+     * @return
+     */
+    @LagouRequestMapping("/query")
+    public String query(HttpServletRequest request, HttpServletResponse response, String name) {
+        return demoService.get(name);
+    }
+
+    @LagouRequestMapping("/zhangsanQuery")
+    @LagouSecurity({"zhangsan"})
+    public String zhangsanQuery(HttpServletRequest request, HttpServletResponse response, String username) {
+        return demoService.get(username);
+    }
+
+    @LagouRequestMapping("/lisiQuery")
+    @LagouSecurity({"lisi"})
+    public String lisiQuery(HttpServletRequest request, HttpServletResponse response, String username) {
+        return demoService.get(username);
+    }
+
+}
